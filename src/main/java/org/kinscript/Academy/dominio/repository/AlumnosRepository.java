8b0d37dbf779b377944f662448e118991259296b
dnmrll <hmuralles-2024370@kinal.edu.gt>
2025-09-16 09:55:05 -0600

    feat: Agregar services y repositories

---
+++ src/main/java/org/kinscript/Academy/dominio/repository/AlumnosRepository.java
@@ -0,0 +1,10 @@
+/*package org.kinscript.Academy.dominio.repository;
+
+public class AlumnosRepository {
+    List<AlumnosDto> obtenerAlumno();
+    public AlumnosDto buscarAlumno(Long codigo);
+    AlumnosDto guardarAlumno(AlumnosDto peliculalumnoDto);
+    AlumnosDto modificarAlumno(Long codigo, ModAlumnosDto  modAlumnos);
+    void eliminarAlumno(Long codigo);
+
+}*/
---
+++ src/main/java/org/kinscript/Academy/dominio/service/AlumnosService.java
@@ -0,0 +1,37 @@
+// package org.kinscript.Academy.dominio.service;
+
+/*
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AlumnosService {
+
+    private final PeliculaRepository peliculaRepository;
+
+    public PeliculaService(PeliculaRepository peliculaRepository){
+        this.peliculaRepository = peliculaRepository;
+    }
+
+    public List<PeliculaDto> obtenerTodo(){
+        return this.peliculaRepository.obtenerTodo();
+    }
+
+    public PeliculaDto buscarPorCodigo(Long codigo){
+        return this.peliculaRepository.buscarPorCodigo(codigo);
+    }
+
+    public PeliculaDto guardarPelicula(PeliculaDto peliculaDto){
+        return this.peliculaRepository.guardarPelicula(peliculaDto);
+    }
+
+    public PeliculaDto modificarPelicula(Long codigo, ModPeliculaDto modPeliculaDto){
+        return this.peliculaRepository.modificarPelicula(codigo, modPeliculaDto);
+    }
+
+    public void eliminarPelicula (Long codigo) {
+        this.peliculaRepository.eliminarPelicula(codigo);
+    }
+
+}*/
