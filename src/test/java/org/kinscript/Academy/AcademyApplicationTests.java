aa41b8e67385c99e219c62b4e799daa949c5cff8
EduardoLG <jlopez-2023086@kinal.edu.gt>
2025-09-16 07:33:31 -0600

    init: initial Commit

---
+++ src/test/java/org/kinscript/Academy/AcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kinscript.Academy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
