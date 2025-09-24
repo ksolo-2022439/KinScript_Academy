9b9275b150fcc819df557b1ee5379960f6eb1546
marcss-bnajera <mbeteta-2024227@kinal.edu.gt>
2025-09-24 09:57:58 -0600

    feat: Agregar IA
    Agregando un generador de actividades dependiendo la materia, grado, y tema para hacer la actividad

---
+++ src/main/java/org/kinscript/Academy/web/controller/ai/AiActividadController.java
@@ -0,0 +1,26 @@
+package org.kinscript.Academy.web.controller.ai;
+
+import org.kinscript.Academy.dominio.dto.SolicitudDto;
+import org.kinscript.Academy.dominio.service.AIServiceColegio;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ai")
+public class AiActividadController {
+
+    private final AIServiceColegio aiService;
+
+    public AiActividadController(AIServiceColegio aiService) {
+        this.aiService = aiService;
+    }
+
+    //Metodo para sugerir la actividad
+    @PostMapping("/sugerir")
+    public ResponseEntity<String> generarSugerenciaActividad(@RequestBody SolicitudDto solicitudDto) {
+        return ResponseEntity.ok(this.aiService.generarActivadad(solicitudDto.preferencias()));
+    }
+}
